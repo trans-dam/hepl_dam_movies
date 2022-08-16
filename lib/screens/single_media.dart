0fbf2fa60ce40ab406ef1440eb3100c96b0cefb1
Daniel Schreurs <daniel.schreurs@hotmail.com>
2022-08-16 19:39:34 +0200

    :bug: Fixing scroll

--- lib/screens/single_media.dart
+++ lib/screens/single_media.dart
@@ -42,13 +42,15 @@ class _SingleMediaState extends State<SingleMedia> {
         .get(Uri.parse(
             'https://api.themoviedb.org/3/movie/${widget._media.movieId}?api_key=fc0b570a0ec2e5a82a99bf4d8340e012&language=fr-fr'))
         .then((response) {
+      print(
+          'https://api.themoviedb.org/3/movie/${widget._media.movieId}?api_key=fc0b570a0ec2e5a82a99bf4d8340e012&language=fr-fr');
       if (response.statusCode == 200) {
         setState(() {
-          this.widget.mediaDetail.fromJson(jsonDecode(response.body));
+          widget.mediaDetail.fromJson(jsonDecode(response.body));
         });
       } else {
         throw Exception(
-            'Oups ! ${response.statusCode} : ${response.reasonPhrase}');
+            'Oups ! ${response.statusCode} : ${response.reasonPhrase} \n widget._media.movieId ! ${widget._media.movieId}');
       }
     }).onError((error, stackTrace) {
       throw Exception(error);
@@ -58,117 +60,119 @@ class _SingleMediaState extends State<SingleMedia> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
-            children: [
-              BackdropPath(widget._media.backdropPath),
-              const MyCloseButton(),
-              Positioned(
-                left: kHorizontalSpacer,
-                bottom: 0,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    SingleMediaPicture(widget._media.posterPath),
-                    Stars((widget._media.voteAverage / 2).floor())
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-                top: kVerticalSpacer,
-                left: kHorizontalSpacer,
-                right: kHorizontalSpacer),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
               children: [
-                Hero(
-                  tag:widget._media.title,
-                  child: Text(
-                    widget._media.title,
-                    style: kLargeTitleStyle,
+                BackdropPath(widget._media.backdropPath),
+                const MyCloseButton(),
+                Positioned(
+                  left: kHorizontalSpacer,
+                  bottom: 0,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      SingleMediaPicture(widget._media.posterPath),
+                      Stars((widget._media.voteAverage / 2).floor())
+                    ],
                   ),
                 ),
-                const SizedBox(
-                  height: kVerticalSpacer / 3,
-                ),
-                Text(
-                  widget.mediaDetail.tagline,
-                  style: kTagLineStyle,
-                ),
-                SizedBox(
-                  height: 20,
-                  width: double.infinity,
-                  child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: widget.mediaDetail.genres.length,
-                    separatorBuilder: (BuildContext context, int index) {
-                      return const Text(
-                        " - ",
-                        style: kGenreStyle,
-                      );
-                    },
-                    itemBuilder: (BuildContext context, int index) {
-                      var g = widget.mediaDetail.genres;
-                      return Text(
-                        "${g[index]['name']}",
-                        style: kGenreStyle,
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: kVerticalSpacer,
-                ),
-                const Text(
-                  "Description",
-                  style: kTitleSection,
-                ),
-                const SizedBox(
-                  height: kVerticalSpacer / 2,
-                ),
-                Text(
-                  widget._media.overview,
-                  style: kBodyLabelStyle,
-                ),
-                const SizedBox(
-                  height: kVerticalSpacer,
-                ),
-                const Text(
-                  "Date de sortie",
-                  style: kTitleSection,
-                ),
-                const SizedBox(
-                  height: kVerticalSpacer / 2,
-                ),
-                Text(
-                  dateFormat.format(widget._media.releaseDate),
-                  style: kBodyLabelStyle,
-                  textAlign: TextAlign.left,
-                ),
-                const SizedBox(
-                  height: kVerticalSpacer,
-                ),
-                const Text(
-                  "Durée",
-                  style: kTitleSection,
-                ),
-                const SizedBox(
-                  height: kVerticalSpacer / 2,
-                ),
-                Text(
-                  '${(widget.mediaDetail.runtime / 60).truncate().toString().padLeft(2, '0')} : ${(widget.mediaDetail.runtime % 60).truncate().toString().padLeft(2, '0')}',
-                  style: kBodyLabelStyle,
-                  textAlign: TextAlign.left,
-                )
               ],
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: kVerticalSpacer,
+                  left: kHorizontalSpacer,
+                  right: kHorizontalSpacer),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Hero(
+                    tag: widget._media.title,
+                    child: Text(
+                      widget._media.title,
+                      style: kLargeTitleStyle,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: kVerticalSpacer / 3,
+                  ),
+                  Text(
+                    widget.mediaDetail.tagline,
+                    style: kTagLineStyle,
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: double.infinity,
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: widget.mediaDetail.genres.length,
+                      separatorBuilder: (BuildContext context, int index) {
+                        return const Text(
+                          " - ",
+                          style: kGenreStyle,
+                        );
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        var g = widget.mediaDetail.genres;
+                        return Text(
+                          "${g[index]['name']}",
+                          style: kGenreStyle,
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: kVerticalSpacer,
+                  ),
+                  const Text(
+                    "Description",
+                    style: kTitleSection,
+                  ),
+                  const SizedBox(
+                    height: kVerticalSpacer / 2,
+                  ),
+                  Text(
+                    widget._media.overview,
+                    style: kBodyLabelStyle,
+                  ),
+                  const SizedBox(
+                    height: kVerticalSpacer,
+                  ),
+                  const Text(
+                    "Date de sortie",
+                    style: kTitleSection,
+                  ),
+                  const SizedBox(
+                    height: kVerticalSpacer / 2,
+                  ),
+                  Text(
+                    dateFormat.format(widget._media.releaseDate),
+                    style: kBodyLabelStyle,
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(
+                    height: kVerticalSpacer,
+                  ),
+                  const Text(
+                    "Durée",
+                    style: kTitleSection,
+                  ),
+                  const SizedBox(
+                    height: kVerticalSpacer / 2,
+                  ),
+                  Text(
+                    '${(widget.mediaDetail.runtime / 60).truncate().toString().padLeft(2, '0')} : ${(widget.mediaDetail.runtime % 60).truncate().toString().padLeft(2, '0')}',
+                    style: kBodyLabelStyle,
+                    textAlign: TextAlign.left,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
